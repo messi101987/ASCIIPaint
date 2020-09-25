cb970dbc7efb899fe6b6f47f300ffa292e09ab7d
g55315 <55315@etu.he2b.be>
2020-09-25 17:35:29 +0200

    first commit

---
+++ src/main/java/g55315/Point.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g55315;
+
+/**
+ *
+ * @author g55315
+ */
+public class Point {
+     private double x;
+    private double y;
+    
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double getX() { return x; }
+    public double getY() { return y; }
+    
+    public Point move(double dx, double dy) {
+        x += dx;
+        y += dy;
+        return this;
+    }
+    
+    @Override
+    public String toString() {
+        return "("+x+", "+y+")";
+    }
+
+}
